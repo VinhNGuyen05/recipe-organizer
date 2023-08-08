d40b1b6503e4277c0d36eda3edc70f9d3b699636
Vinh Nguyễn <vinhnguyen50501@gmail.com>
2023-08-08 22:56:29 +0700

    setup firebase to deploy

---
+++ src/services/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBP7wwbFT9wxZbr-JmKi3xhXbheyenmEGo",
+    authDomain: "recipe-organizer-swp391.firebaseapp.com",
+    projectId: "recipe-organizer-swp391",
+    storageBucket: "recipe-organizer-swp391.appspot.com",
+    messagingSenderId: "925656558059",
+    appId: "1:925656558059:web:3d0fe95ae7fd5b650e11ae",
+    measurementId: "G-GNPR3BQQ2R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
